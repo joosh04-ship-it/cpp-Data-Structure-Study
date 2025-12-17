031f71ceb4aa75dd9c546449bc59743d7eb2f7dc
PyeongJunPark <pjme2004@naver.com>
2025-12-18 03:33:13 +0900

    1W4dPJ

---
+++ week01/Day4/Park/chapter5.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+class shallowStock {
+private:
+	string Ticker;
+	int* price;
+public:
+	shallowStock(string T, int* p){
+		Ticker = T;
+		price = new int[5];
+		copy(p, p + 5, price);
+	}//일반 생성자
+	~shallowStock() {
+		//delete[] price;//이러면 2중으로 같은 포인터를 delete 하게 되어서 프로그램이 터져버림
+	}
+	int* getPrice() {
+		return price;
+	}
+	void setPrice(int idx, int p) {
+		this->price[idx] = p;
+	}
+};
+class DeepStock {
+private:
+	string Ticker;
+	int* price;
+public:
+	DeepStock(string T, int* p) {//일반 생성자
+		Ticker = T;
+		price = new int[5];
+		copy(p, p + 5, price);
+	}
+	//복사생성자
+	DeepStock(const DeepStock& stock) {//컨스트 레퍼런스 사용 이유 : 원본의 값을 가져와야하지만, 직접 들고오면 CLASS이기때문에 용량이 커서 주소만 가져오되
+										//원본 값의 변경을 허용하지 않기 위함
+		Ticker = stock.Ticker;
+		price = new int[5];
+		copy(stock.price, stock.price + 5, price);
+	}
+	~DeepStock() {
+		delete[] price;
+	}
+	int* getPrice() {
+		return price;
+	}
+	void setPrice(int idx, int p) {
+		this->price[idx] = p;
+	}
+};
+int main(){
+//복사생성자
+	//이미 존재하는 객체로 새 객체를 만들고자할 때 사용
+	//기본 복사 생성자 사용 시  얕은 복사 수행
+	//얕은 복사는 레퍼런스나 포인터 복사 시 의도치 않은 문제 발생 시킬 수 있음
+	int tslaPrices[5] = { 1,2,3,4,5 };
+	shallowStock A("TSLA", tslaPrices);//일반생성자 호출
+	shallowStock B = A;//기본 복사생성자 호출
+	B.setPrice(2, 100000);
+	cout << A.getPrice()[2] << endl;//B의 정보를 바꾸었는데 A도 바뀌어버림
+	//깊은 복사
+	int nvdaPrices[5] = { 6,7,8,9,10 };
+	DeepStock C("NVDA", nvdaPrices);//일반생성자 호출
+	DeepStock D = C;//복사생성자 호출
+	D.setPrice(1, 0);
+	cout << C.getPrice()[1] << endl;//D의 정보를 바꾸었지만 C는 바뀌지 않음
+}
---
+++ week01/Day4/Park/chapter6.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+using namespace std;
+//friend
+class LEE;
+class PARK;
+void voiceFishing(PARK&);
+class JOO {
+private:
+	void JooshPrivate() {
+		cout << "주승현 비밀 접근" << endl;
+	}
+	friend class LEE;// 특정 클래스를 프랜드로 지정 시 그 클래스에서 이 클래스의 private과 protected를 접근할 수 있음
+};
+class PARK {
+private:
+	friend void voiceFishing(PARK&);//전역함수도 프랜드로 지정 가능
+	void ParkPJPrivate() {
+		cout << "박평준은 비밀이 없음" << endl;
+	}
+public:
+	void hackingJS(LEE&);
+};
+class LEE {
+private:
+	void LeeJSPrivate() {
+		cout << "이진서 비밀 정보" << endl;
+	}
+	friend void PARK::hackingJS(LEE& lee);//다른 클래스의 멤버함수 하나만 지정해서 친구로 지정도 할 수 있음
+public:
+	JOO joo;
+	void hackingSH() {
+		joo.JooshPrivate();
+	}
+
+};
+void PARK::hackingJS(LEE& lee) {
+	lee.LeeJSPrivate();
+}
+void voiceFishing(PARK& park) {
+	park.ParkPJPrivate();
+}
+int main()
+{
+//friend
+	LEE JS;
+	JS.hackingSH();
+	PARK PJ;
+	PJ.hackingJS(JS);
+	voiceFishing(PJ);
+}
+//mutable: const 함수에서도 변경 가능
+//static함수 : static 변수만 내부에 사용 가능
+//class명::함수명으로 사용 가능
