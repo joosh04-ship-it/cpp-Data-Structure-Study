7ba76350e4f7d4f95d2889e9b3490b44b6216130
04xio <dlwlstj4321@gmail.com>
2026-01-09 12:16:57 +0900

    4w3dJS

---
+++ week04/Day3/Lee/chapter15-2.cpp
@@ -0,0 +1,91 @@
+﻿//예외 클래스와 예외 객체
+#include <iostream>
+#include <cstring>
+#include <new>
+using namespace std;
+
+//상속 관계가 있는 예외 클래스
+
+class MyException {
+public:
+    virtual void ShowInf() = 0;
+};
+
+class PJ : public MyException {
+private:
+    string name;
+public:
+    PJ(string name) : name(name) {}
+    void ShowInf() {
+        cout << "[평주니 예외 메시지: " << name << "(이)는 출입 불가]" << endl;
+    }
+};
+
+class SH : public MyException {
+private:
+    string name;
+public:
+    SH(string name) : name(name) {}
+    void ShowInf() {
+        cout << "[승혀니 예외 메시지: " << name << "(이)는 출입 불가]" << endl;
+    }
+};
+
+class GoPeople {
+private:
+    string name;
+public:
+    GoPeople(string n) : name(n) {}
+    void NoEnter(string n) throw (PJ) {
+        if (n == "평준") {
+            PJ expn(n);
+            throw expn;
+        }
+        else if (n == "승현") {
+            SH expn(n);
+            throw expn;
+        }
+        cout << n<<"이라는 친구는 통과" << endl;
+    }
+};
+
+
+
+int main()
+{
+    GoPeople myacc("친구");
+    string who;
+    while (1) {
+
+        if (who == "끝내기")
+
+        cin >> who;
+        try {
+            myacc.NoEnter(who);
+            
+        }
+        catch (MyException& expn) {
+            
+            expn.ShowInf();
+        }
+    }
+    
+
+    //프로그래머가 정의하지 않아도 발생하는 예외
+    /*
+    int num = 0;
+
+    try {
+        while (1) {
+            num++;
+            cout << num << "번째 할당 시도" << endl;
+            new int[10000][10000];
+        }
+    }
+
+    catch (bad_alloc& bad) {
+        cout << bad.what() << endl;
+        cout << "더 이상 할당 불가!" << endl;
+    }
+    */
+}
---
+++ week04/Day3/Lee/chapter15.cpp
@@ -0,0 +1,112 @@
+﻿#include <iostream>
+#include <cstring>
+#include <cmath>
+using namespace std;
+
+//예외를 던질 수도 있음
+void Divide(int num1, int num2) {
+    try {
+        if (num2 == 0)
+        //예외처리에 대한 책임은 함수를 호출한 영역을 넘어가게 됨
+        throw num2; //예외를 던지고,
+    }
+    catch (int expn) {
+        cout << "함수 내에서 먼저 잡기!" << endl;
+        throw;
+    }
+    cout << "곱셈의 몫: " << num1 * num2 << endl;
+}
+
+//스택 풀기
+void Func3() {
+    cout << "Func3 start" << endl;
+    throw 1;  // 예외 발생
+    cout << "Func3 end" << endl;
+}
+
+void Func2() {
+    cout << "Func2 start" << endl;
+    Func3();
+    cout << "Func2 end" << endl;
+}
+
+void Func1() {
+    cout << "Func1 start" << endl;
+    Func2();
+    cout << "Func1 end" << endl;
+}
+
+//전달되는 예외의 명시
+int StoI(char* str) throw (int, char) {
+    int len = strlen(str);
+    int num = 0;
+
+    if (len != 0 && str[0] == '0')
+        throw 0;
+    for (int i = 0; i < len; i++) {
+        if (str[i] < '0' || str[i]>'9')
+            throw str[i];
+        num += (int)(pow((double)10, (len - 1) - i) * (str[i] + (7 - '7')));
+    }
+    return num;
+}
+
+int main() {
+    int num1, num2;
+    string name;
+    cout << "두 개의 숫자 입력: ";
+    cin >> num1 >> num2;
+
+    try { //예외를 발견하고,
+        Divide(num1, num2);
+        if (num2 == 0)
+            throw num2; //예외를 던지고,
+        cout << "나눗셈의 몫: " << num1 / num2 << endl;
+        cout << "나눗셈의 나머지: " << num1 % num2 << endl;
+    }
+    catch (int expn) { //예외를 잡는다!
+        cout << "제수는 " << expn << "이 될 수 없습니다." << endl;
+        cout << "프로그램을 다시 실행하세요." << endl;
+    }
+    cout << "-----------------------------" << endl;
+    try {
+        Func1();
+    }
+    catch (int e) {
+        cout << "예외 잡음: " << e << endl;
+    }
+    cout << "-----------------------------" << endl;
+    char str1[100];
+    char str2[200];
+    while (1) {
+        cout << "두 개의 숫자 입력: ";
+        cin >> str1 >> str2;
+        
+        //하나의 try 블록에 다수의 catch 블록 가능!!
+        try {
+            int res = StoI(str1) + StoI(str2);
+            cout << str1 << " + " << str2 << " = " << res << endl;
+            break;
+        }
+        catch (char ch)
+        {
+            cout << "문자 " << ch << "가 입력되었습니다" << endl;
+            cout << "재입력 진행합니다." << endl << endl;
+
+        }
+        catch (int expn) {
+            if (expn == 0)
+                cout << "0으로 시작하는 숫자는 입력불가" << endl;
+            else
+                cout << "비정상적 입력이 이루어졌습니다." << endl;
+            cout << "재입력 진행합니다." << endl << endl;
+        }
+    }
+    cout << "프로그램을 종료합니다." << endl;
+
+    return 0;
+}
+
+
+
+
