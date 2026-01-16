7d551d0d08040f395acf240413564108c511db15
04xio <dlwlstj4321@gmail.com>
2026-01-16 12:17:49 +0900

    5w2djs

---
+++ week05/Day1/Lee/Stack_test.cpp
@@ -0,0 +1,121 @@
+﻿#include <iostream>
+using namespace std;
+
+template<typename T>
+class Stack {
+private:
+    T *data;
+    int idx = 0;
+    int size;
+public:
+    Stack(int size) : idx(0), size(size <= 0 ? 1 : size) {
+        if (size <= 0) {
+            cout << "size는 0이하가 될 수 없습니다!(size 1로 교체)" << endl;
+        }
+        data = new T[this->size];
+    }
+
+
+    Stack(const Stack& other) {
+        size = other.size;
+        idx = other.idx;
+
+        data = new T[size];
+        for (int i = 0; i < idx; i++) {
+            data[i] = other.data[i];
+        }
+    }
+
+    Stack& operator=(const Stack& other) {
+        if (this == &other)
+            return *this;
+
+        delete[] data;   // 기존 메모리 해제
+
+        size = other.size;
+        idx = other.idx;
+
+        data = new T[size];
+        for (int i = 0; i < idx; i++) {
+            data[i] = other.data[i];
+        }
+
+        return *this;
+    }
+
+    /*
+    Stack(const Stack&) = delete;
+    Stack& operator=(const Stack&) = delete;
+    */
+
+
+
+    bool empty() const {
+        if (idx == 0)
+            return true;
+        else
+            return false;
+    }
+
+    void DynamicStack() {
+        if (idx >= size) {
+            size *= 2;
+            T* newdata = new T[size];
+            for (int i = 0; i < idx; i++) {
+                newdata[i] = data[i];
+            }
+
+            delete[] data;
+            data = newdata;
+            
+
+        }
+    }
+
+    void Push(T val) {
+        DynamicStack();
+        data[idx] = val;
+        idx++;
+        
+    }
+
+    T Pop() {
+        if (empty()) {
+            cout << "스택이 이미 비어있습니다!(Pop불가)" << endl;
+            return T();
+        }
+        return data[--idx];
+    }
+    
+    T Top() const {
+        if (empty()) {
+            cout << "스택이 이미 비어있습니다!(Top불가)" << endl;
+            return T();
+        }
+        return data[idx-1];
+    }
+    int checksize() const{
+        return idx;
+    }
+
+    ~Stack() {
+        delete[] data;
+    }
+    
+};
+
+int main()
+{
+    Stack<int> s(0);
+    s.Push(1);
+    s.Push(2);
+    s.Push(3);
+    s.Push(4);
+    s.Push(5);
+    s.Push(6);
+    cout << s.Pop(); 
+    cout << endl;
+    cout<<s.Top();
+}
+
+
---
+++ week05/Day2/Lee/queue.cpp
@@ -0,0 +1,109 @@
+﻿// queue.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+
+template<typename T>
+class Queue {
+private:
+    T* data;
+    int front = 0;
+    int rear = 0;
+    int capacity;
+public:
+    Queue(int sz) : front(0), rear(0), capacity(sz <= 0 ? 2 : sz + 1) {
+        if (sz <= 0) {
+            cout << "capacity가 0이하는 에바여서 2로 바꿔줌 ㅎㅎ" << endl;
+        }
+        data = new T[capacity];
+    }
+
+    bool Empty() const {
+        return front == rear;
+    }
+
+    Queue(const Queue&) = delete;
+    Queue& operator=(const Queue&) = delete;
+
+    void Expand() {
+        if ((rear + 1) % capacity == front) {
+            int oldCap = capacity;
+            capacity *= 2;
+            T* cpydata = new T[capacity];
+            int num = 0;
+            for (int i = front; i != rear; i = (i+1) % oldCap) {
+                cpydata[num] = data[i];
+                num++;
+                
+            }
+
+            front = 0;
+            rear = num;
+
+            delete[] data;
+            data = cpydata;
+        }
+    }
+
+    void Enqueue(T val) {
+        Expand();
+        data[rear] = val;
+        rear = (rear + 1) % capacity;
+    }
+    T Dequeue() {
+        if (Empty()) {
+            cout << "비어있따. 끄지라" << endl;
+            return T();
+        }
+
+        T res = data[front];
+        front = (front + 1) % capacity;
+        return res;
+    }
+
+    int checksize() const {
+        return (rear - front + capacity) % capacity;
+    }
+
+    ~Queue() {
+        delete[] data;
+    }
+};
+
+int main() {
+    Queue<int> q(3);   // 내부 capacity = 4 (한 칸 비움)
+
+    cout << "=== Enqueue 1 ~ 6 ===\n";
+    for (int i = 1; i <= 6; i++) {
+        q.Enqueue(i);
+        cout << "push " << i
+            << " | size = " << q.checksize() << '\n';
+    }
+
+    cout << "\n=== Dequeue 3 ===\n";
+    for (int i = 0; i < 3; i++) {
+        cout << "pop " << q.Dequeue()
+            << " | size = " << q.checksize() << '\n';
+    }
+
+    cout << "\n=== Enqueue 7 ~ 10 ===\n";
+    for (int i = 7; i <= 10; i++) {
+        q.Enqueue(i);
+        cout << "push " << i
+            << " | size = " << q.checksize() << '\n';
+    }
+
+    cout << "\n=== Dequeue all ===\n";
+    while (!q.Empty()) {
+        cout << q.Dequeue() << ' ';
+    }
+    cout << "\n";
+
+    return 0;
+}
+
+
+
+
+
